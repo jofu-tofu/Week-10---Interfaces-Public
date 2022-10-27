5ed4c85caaed72160a30b3b07353da52fdc42699
Joshua Fu <joshua.fu264@gmail.com>
2022-10-26 21:45:12 -0400

    Week 10 GUI
    GUI Application

--- Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
+++ Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
@@ -7,18 +7,20 @@ public class ChangeTextValue : MonoBehaviour
 {
     // DIRECTIONS: The goal of this script is to show the Y coordinate of the cube on
     // the screen. You will need to add some stuff to this script to make it work...
+    [SerializeField] GameObject obj;
 
     TextMeshProUGUI textMeshProUGUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        textMeshProUGUI.text = obj.transform.position[1].ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textMeshProUGUI.text = obj.transform.position[1].ToString();
     }
 }
